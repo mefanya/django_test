bfef6167792078c191cb49119e8aca356f981bea
mefanya <ms1604@bk.ru>
2024-08-27 16:20:51 +0300

    Настройка подключения к БД

--- config/settings.py
+++ config/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'orm',
+        'USER': 'postgres',
+        'PASSWORD': 'qwerty123'
     }
 }
 
