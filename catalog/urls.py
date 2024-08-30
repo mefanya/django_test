65626f67f25295bf817afc780bc5c12cff22fddf
mefanya <ms1604@bk.ru>
2024-08-31 00:11:34 +0300

    Добавлена возможность, добавлять продукты

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import products_list, product_detail
+from catalog.views import products_list, product_detail, add_product
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", products_list, name='products_list'),
-    path("product/<int:pk>/", product_detail, name='product_detail'),
+    path("", products_list, name="products_list"),
+    path("product/<int:pk>/", product_detail, name="product_detail"),
+    path("add_product/", add_product, name="add_product"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -8,7 +8,26 @@ def products_list(request):
     context = {"products": products}
     return render(request, "products_list.html", context)
 
+
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     context = {"product": product}
     return render(request, "product_detail.html", context)
+
+
+def add_product(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        description = request.POST.get("description")
+        is_available = request.POST.get("is_available") == "true"
+        image = request.FILES.get("image")
+
+        Product.objects.create(
+            name=name,
+            price=price,
+            description=description,
+            is_available=is_available,
+            image=image,
+        )
+    return render(request, "add_product.html")
