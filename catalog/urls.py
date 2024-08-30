df711e2c8768b1a81017c49efb9310d5276cbc8a
mefanya <ms1604@bk.ru>
2024-08-30 22:48:26 +0300

    Сделал шаблоны и реализовал переходы на страницы. Добавил меню

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,10 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import index
+from catalog.views import products_list, product_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", index),
+    path("", products_list, name='products_list'),
+    path("product/<int:pk>/", product_detail, name='product_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from catalog.models import Product
 
 
-def index(request):
-    return render(request, "base.html")
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "products_list.html", context)
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, "product_detail.html", context)
